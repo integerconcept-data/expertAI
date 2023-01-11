346658eff3a7f06b587fdec62d3c16d3ac3b0889
IntegerConcept <sesan2ky@gmail.com>
2023-01-11 14:33:10 +0100

    update the server url

--- client/scripts.js
+++ client/scripts.js
@@ -84,7 +84,7 @@ const handleSubmit=async (e)=>{
 
   // fetch data from server bot's response
 
-  const response = await fetch('http://localhost:5000/',{
+  const response = await fetch('https://expertbot.onrender.com/',{
     method:'POST',
     headers:{
       'content-Type': 'application/json'
